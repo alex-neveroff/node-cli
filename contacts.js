b510e13625f28707081cb050e75e939eb47d1700
Alexandr Neveroff <neveroff1976@gmail.com>
2023-07-07 20:00:41 +0300

    contactsPath

--- contacts.js
+++ contacts.js
@@ -1,7 +1,7 @@
 const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
-const contactsPath = path.join(__dirname, "db/contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 // Вивести список усіх контактів
 const getListContacts = async () => {
